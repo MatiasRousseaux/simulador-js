ffa8ea7e697e1de737922eab755f1f6e09284290
Matias Rousseaux <matiasrousseaux@gmail.com>
2024-03-12 22:29:45 +0100

    entrega3

--- js/main.js
+++ js/main.js
@@ -1,156 +1,219 @@
-//ACCESO
-let usuario = "Matias";
-let hashedPassword = "1234";
-let check = "Buenos Aires";
+document.addEventListener('DOMContentLoaded', () => {
 
-let ingresouser, ingresopass, checkIngreso;
-let intentos = 3;
-let acceso = false;
+    // Variables
+    const baseDeDatos = [
+        {
+            id: 1,
+            nombre: "Auriculares Inalambricos",
+            stock: 1,
+            marca: "Sony",
+            precio: 26500,
+            img: "auriculares.jpg"
+        },
+        {
+            id: 2,
+            nombre: "Conjunto Hombre",
+            stock: 2,
+            marca: "Adidas",
+            precio: 65000,
+            img: "conjunto.jpg"
+        },
+        {
+            id: 3,
+            nombre: "Gorra Azul",
+            stock: 1,
+            marca: "LA",
+            precio: 15000,
+            img: "gorra.jpg"
+        },
+        {
+            id: 4,
+            nombre: "Perfume",
+            stock: 1,
+            marca: "Dolce Gabbana",
+            precio: 50000,
+            img: "perfume.jpg"
+        },
+        {
+            id: 5,
+            nombre: "Reloj",
+            stock: 4,
+            marca: "Tissot",
+            precio: 230000,
+            img: "reloj.jpg"
+        },
+        {
+            id: 6,
+            nombre: "Zapatillas",
+            stock: 2,
+            marca: "Adidas",
+            precio: 105000,
+            img: "zapatillas.jpg"
+        },
+    ];
 
-for (let i = 0; i < intentos; i++) {
-    ingresouser = prompt("Ingresa tu usuario (Matias)");
-    ingresopass = prompt("Ingresa tu contraseña (1234)");
-    checkIngreso = prompt("¿Cuál es la capital de Argentina? (Buenos Aires)");
+    let carrito = [];
+    const divisa = '€';
+    const DOMitems = document.querySelector('#items');
+    const DOMcarrito = document.querySelector('#carrito');
+    const DOMtotal = document.querySelector('#total');
+    const DOMbotonVaciar = document.querySelector('#boton-vaciar');
+    const miLocalStorage = window.localStorage;
 
-    let isValidCredentials = ingresouser === usuario && validatePassword(ingresopass, hashedPassword);
+    // Funciones
 
-    if (isValidCredentials && checkIngreso === check) {
-        acceso = true;
-        alert("Bienvenido " + usuario);
-        break;
-    } else {
-        alert("Credenciales incorrectas. Por favor, inténtalo de nuevo. Tienes " + (intentos - i - 1) + " intentos");
-    }
-}
-
-if (!acceso) {
-    alert("Acceso denegado. Has agotado todos los intentos.");
-}
-
-
-function validatePassword(inputPassword, storedHashedPassword) {
-    return inputPassword === storedHashedPassword;
-}
-
-const carrito = [];
-
-function Producto(id, tipo, stock, marca, precio) {
-    this.id = id;
-    this.tipo = tipo;
-    this.stock = stock;
-    this.marca = marca;
-    this.precio = precio;
-    this.mostrarInfo = function () {
-        return `Tipo: ${this.tipo}, Stock: ${this.stock}, Marca: ${this.marca}, Precio: ${this.precio}`;
-    };
-    this.mostrarStock = function () {
-        return this.stock;
-    };
-}
-
-
-
-const listaProductos = [];
-
-
-for (let i = 0; i < 1; i++) {
-    const producto1 = new Producto("Auriculares Inalambricos", 1, "Sony", "$26500");
-    const producto2 = new Producto("Conjunto Hombre", 2, "Adidas", "$65000");
-    const producto3 = new Producto("Gorra Azul", 1, "LA", "$15000");
-    const producto4 = new Producto("Perfume", 1, "Dolce Gabbana", "$50000");
-    const producto5 = new Producto("Reloj", 4, "Tissot", "$230000");
-    const producto6 = new Producto("Zapatillas", 2, "Adidas", "$105000");
-
-    listaProductos.push(producto1, producto2, producto3, producto4, producto5, producto6);
-}
-
-console.log(listaProductos);
-
-
-const productos = [
-    {
-        id: 1,
-        nombre: "Auriculares Inalambricos",
-        stock: 1,
-        marca: "Sony",
-        precio: 26500,
-        img: "auriculares.jpg"
-    },
-    {
-        id: 2,
-        nombre: "Conjunto Hombre",
-        stock: 2,
-        marca: "Adidas",
-        precio: 65000,
-        img: "conjunto.jpg"
-    },
-    {
-        id: 3,
-        nombre: "Gorra Azul",
-        stock: 1,
-        marca: "LA",
-        precio: 15000,
-        img: "gorra.jpg"
-    },
-    {
-        id: 4,
-        nombre: "Perfume",
-        stock: 1,
-        marca: "Dolce Gabbana",
-        precio: 50000,
-        img: "perfume.jpg"
-    },
-    {
-        id: 5,
-        nombre: "Reloj",
-        stock: 4,
-        marca: "Tissot",
-        precio: 230000,
-        img: "reloj.jpg"
-    },
-    {
-        id: 6,
-        nombre: "Zapatillas",
-        stock: 2,
-        marca: "Adidas",
-        precio: 105000,
-        img: "zapatillas.jpg"
-    },
-]
-console.log(productos.length);
-
- 
-function mostrarDatos(array) {
-    for (let i = 0; i < array.length; i++) {
-        console.log(array[i]);
+    /**
+    * Dibuja todos los productos a partir de la base de datos. No confundir con el carrito
+    */
+    function renderizarProductos() {
+        baseDeDatos.forEach((info) => {
+            // Estructura
+            const miNodo = document.createElement('div');
+            miNodo.classList.add('card', 'col-sm-4');
+            // Body
+            const miNodoCardBody = document.createElement('div');
+            miNodoCardBody.classList.add('card-body');
+            // Titulo
+            const miNodoTitle = document.createElement('h5');
+            miNodoTitle.classList.add('card-title');
+            miNodoTitle.textContent = info.nombre;
+            // Imagen
+            const miNodoImagen = document.createElement('img');
+            miNodoImagen.classList.add('img-fluid');
+            miNodoImagen.setAttribute('src', info.imagen);
+            // Precio
+            const miNodoPrecio = document.createElement('p');
+            miNodoPrecio.classList.add('card-text');
+            miNodoPrecio.textContent = `${info.precio}${divisa}`;
+            // Boton
+            const miNodoBoton = document.createElement('button');
+            miNodoBoton.classList.add('btn', 'btn-primary');
+            miNodoBoton.textContent = '+';
+            miNodoBoton.setAttribute('marcador', info.id);
+            miNodoBoton.addEventListener('click', anyadirProductoAlCarrito);
+            // Insertamos
+            miNodoCardBody.appendChild(miNodoImagen);
+            miNodoCardBody.appendChild(miNodoTitle);
+            miNodoCardBody.appendChild(miNodoPrecio);
+            miNodoCardBody.appendChild(miNodoBoton);
+            miNodo.appendChild(miNodoCardBody);
+            DOMitems.appendChild(miNodo);
+        });
     }
 
-
-
-}
-
-mostrarDatos(productos)
-
-
-//AGREGAR PRODUCTOS AL STOCK
-
-function Producto(nombre, stock, marca, precio, img){
-    this.nombre = nombre;
-    this.stock = stock;
-    this.marca = marca;
-    this.precio = parseFloat (precio);
-    if(img)
-    {
-        this.img = img;
-    } else {
-        this.img = "httpps://via.placeholder.com/300";
+    /**
+    * Evento para añadir un producto al carrito de la compra
+    */
+    function anyadirProductoAlCarrito(evento) {
+        // Anyadimos el Nodo a nuestro carrito
+        carrito.push(evento.target.getAttribute('marcador'))
+        // Actualizamos el carrito
+        renderizarCarrito();
+        // Actualizamos el LocalStorage
+        guardarCarritoEnLocalStorage();
     }
-}
-console.log(productos);
 
-//DE ESTA FORMA AGREGO PRODUCTOS
-const newProducto = new Producto ("Reloj", 2, "swatch", 190000,"img.png")
-productos.push(newProducto);
+    /**
+    * Dibuja todos los productos guardados en el carrito
+    */
+    function renderizarCarrito() {
+        // Vaciamos todo el html
+        DOMcarrito.textContent = '';
+        // Quitamos los duplicados
+        const carritoSinDuplicados = [...new Set(carrito)];
+        // Generamos los Nodos a partir de carrito
+        carritoSinDuplicados.forEach((item) => {
+            // Obtenemos el item que necesitamos de la variable base de datos
+            const miItem = baseDeDatos.filter((itemBaseDatos) => {
+                // ¿Coincide las id? Solo puede existir un caso
+                return itemBaseDatos.id === parseInt(item);
+            });
+            // Cuenta el número de veces que se repite el producto
+            const numeroUnidadesItem = carrito.reduce((total, itemId) => {
+                // ¿Coincide las id? Incremento el contador, en caso contrario no mantengo
+                return itemId === item ? total += 1 : total;
+            }, 0);
+            // Creamos el nodo del item del carrito
+            const miNodo = document.createElement('li');
+            miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
+            miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${miItem[0].precio}${divisa}`;
+            // Boton de borrar
+            const miBoton = document.createElement('button');
+            miBoton.classList.add('btn', 'btn-danger', 'mx-5');
+            miBoton.textContent = 'X';
+            miBoton.style.marginLeft = '1rem';
+            miBoton.dataset.item = item;
+            miBoton.addEventListener('click', borrarItemCarrito);
+            // Mezclamos nodos
+            miNodo.appendChild(miBoton);
+            DOMcarrito.appendChild(miNodo);
+        });
+        // Renderizamos el precio total en el HTML
+        DOMtotal.textContent = calcularTotal();
+    }
 
+    /**
+    * Evento para borrar un elemento del carrito
+    */
+    function borrarItemCarrito(evento) {
+        // Obtenemos el producto ID que hay en el boton pulsado
+        const id = evento.target.dataset.item;
+        // Borramos todos los productos
+        carrito = carrito.filter((carritoId) => {
+            return carritoId !== id;
+        });
+        // volvemos a renderizar
+        renderizarCarrito();
+        // Actualizamos el LocalStorage
+        guardarCarritoEnLocalStorage();
 
- 
+    }
+
+    /**
+     * Calcula el precio total teniendo en cuenta los productos repetidos
+     */
+    function calcularTotal() {
+        // Recorremos el array del carrito
+        return carrito.reduce((total, item) => {
+            // De cada elemento obtenemos su precio
+            const miItem = baseDeDatos.filter((itemBaseDatos) => {
+                return itemBaseDatos.id === parseInt(item);
+            });
+            // Los sumamos al total
+            return total + miItem[0].precio;
+        }, 0).toFixed(2);
+    }
+
+    /**
+    * Varia el carrito y vuelve a dibujarlo
+    */
+    function vaciarCarrito() {
+        // Limpiamos los productos guardados
+        carrito = [];
+        // Renderizamos los cambios
+        renderizarCarrito();
+        // Borra LocalStorage
+        localStorage.clear();
+
+    }
+
+    function guardarCarritoEnLocalStorage () {
+        miLocalStorage.setItem('carrito', JSON.stringify(carrito));
+    }
+
+    function cargarCarritoDeLocalStorage () {
+        // ¿Existe un carrito previo guardado en LocalStorage?
+        if (miLocalStorage.getItem('carrito') !== null) {
+            // Carga la información
+            carrito = JSON.parse(miLocalStorage.getItem('carrito'));
+        }
+    }
+
+    // Eventos
+    DOMbotonVaciar.addEventListener('click', vaciarCarrito);
+
+    // Inicio
+    cargarCarritoDeLocalStorage();
+    renderizarProductos();
+    renderizarCarrito();
+});
